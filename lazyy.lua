e4a111256ef3fdf8f36e2b32bb88e3194838de09
ntt419.03@ <136401277+gomeo03@users.noreply.github.com>
2023-10-16 14:30:59 +0700

    Create lazyy.lua

---
+++ lazyy.lua
@@ -0,0 +1,32 @@
+## File Structure
+
+<pre>
+~/.config/nvim
+├── lua
+│   └── lazyvim
+│       ├── config
+│       │   ├── autocmds.lua
+│       │   ├── keymaps.lua
+│       │   ├── lazy.lua
+│       │   ├── options.lua
+│       │   └── settings.lua
+│       ├── plugins
+│       │   ├── lsp
+│       │   │   ├── format.lua
+│       │   │   ├── init.lua
+│       │   │   ├── keymaps.lua
+│       │   │   └── servers.lua
+│       │   ├── coding.lua
+│       │   ├── colorscheme.lua
+│       │   ├── config.lua
+│       │   ├── editor.lua
+│       │   ├── treesitter.lua
+│       │   ├── ui.lua
+│       │   └── util.lua
+│       └── util.lua
+├── init.lua
+├── lazy-lock.json
+├── LICENSE
+├── README.md
+└── stylua.toml
+</pre>
